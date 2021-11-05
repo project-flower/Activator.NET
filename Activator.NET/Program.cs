e7c69a652f661c9a0e3621f716a347b4abfd7302
Yuki Nishina <project_flower@outlook.com>
2021-11-05 23:42:39 +0900

    多重起動をできないようにした。

--- Activator.NET/Program.cs
+++ Activator.NET/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Activator.NET
@@ -14,9 +14,39 @@ namespace Activator.NET
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            string guid = Assembly.GetExecutingAssembly().GetCustomAttribute<GuidAttribute>().Value;
+            var mutex = new Mutex(false, guid);
+            bool hasHandle = false;
+
+            try
+            {
+                Application.EnableVisualStyles();
+
+                try
+                {
+                    if (!mutex.WaitOne(0, false))
+                    {
+                        MessageBox.Show("多重起動はできません。", "Activator.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                catch (AbandonedMutexException)
+                {
+                }
+
+                hasHandle = true;
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FormMain());
+            }
+            finally
+            {
+                if (hasHandle)
+                {
+                    mutex.ReleaseMutex();
+                }
+
+                mutex.Close();
+            }
         }
     }
 }
